8d4007b541f6d503575c92724fd72b8c275848fc
Sqv51 <sykaraagac@hotmail.com>
2022-07-06 11:39:53 +0300

    vending class ara commit

--- vending_machine.cpp
+++ vending_machine.cpp
@@ -1,6 +1,32 @@
 #include "vending_machine.h"
+#include <iostream>
 
-Vending_machine::Vending_machine()
+Vending_machine::Vending_machine(std::string serviceID, std::string serviceType)
+
 {
+    Vending_machine::serviceID = serviceID;
+    Vending_machine::serviceType = serviceType;
+}
+std::string Vending_machine::buy(int order){
+    if(order > vlist.size()){
+        return "Invalid order";
+            }
+    else if(order<4 && order >=0){
+       if(Vending_machine::vslot.at(order)>0){
+       vslot.at(order)--;
+       return vlist.at(order);
+       }
+       else{
+            return ("Sorry we are out of " + vlist.at(order));
+       }
+    }
+    else{
+        if(cups>0){
+            cups--;
+            return vlist.at(order);
+        }
+         else{return "sorry we are out of cups";}
 
+
+    }
 }
--- vending_machine.h
+++ vending_machine.h
@@ -1,15 +1,22 @@
 #ifndef VENDING_MACHINE_H
 #define VENDING_MACHINE_H
 #include "service.h"
-//2 strcuks to be added
+#include <vector>
+
 
 class Vending_machine: public Service
 {
 public:
-    Vending_machine();
+    Vending_machine(std::string serviceID,std::string serviceType);
+    std::string buy(int);
+
+
+
 private:
-    int cups;
-    int Snancks;
+    int cups=100;
+    std::vector<int>vslot{20,20,20,20};
+    std::vector<std::string>vlist{"[1]Çikolata","[2]Gazoz","[3]Bisküvi","[4]Popkek","[5]Espresso","[6]Latte"};
+
 };
 
 #endif // VENDING_MACHINE_H
